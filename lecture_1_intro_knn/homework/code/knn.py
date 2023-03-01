02c6ff32c5acc6b58669280b922a454a20e16075
tatanazubko <pochta.tz@gmail.com>
2023-03-02 00:11:54 +0100

    HomeWork-1

---
+++ lecture_1_intro_knn/homework/code/knn.py
@@ -0,0 +1,148 @@
+import numpy as np
+from scipy.spatial import distance as dist
+
+class KNNClassifier:
+    """
+    K-neariest-neighbor classifier using L1 loss
+    """
+    
+    def __init__(self, k=1):
+        self.k = k
+    
+
+    def fit(self, X, y):
+        self.train_X = X
+        self.train_y = y
+
+
+    def predict(self, X, n_loops=0):
+        """
+        Uses the KNN model to predict clases for the data samples provided
+        
+        Arguments:
+        X, np array (num_samples, num_features) - samples to run
+           through the model
+        num_loops, int - which implementation to use
+
+        Returns:
+        predictions, np array of ints (num_samples) - predicted class
+           for each sample
+        """
+        
+        if n_loops == 0:
+            distances = self.compute_distances_no_loops(X)
+        elif n_loops == 1:
+            distances = self.compute_distances_one_loops(X)
+        else:
+            distances = self.compute_distances_two_loops(X)
+        
+        if len(np.unique(self.train_y)) == 2:
+            return self.predict_labels_binary(distances)
+        else:
+            return self.predict_labels_multiclass(distances)
+
+
+    def compute_distances_two_loops(self, X):
+        """
+        Computes L1 distance from every sample of X to every training sample
+        Uses simplest implementation with 2 Python loops
+
+        Arguments:
+        X, np array (num_test_samples, num_features) - samples to run
+        
+        Returns:
+        distances, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        """
+        
+        distances = np.zeros((X.shape[0], self.train_X.shape[0]))
+        # Create an array of all zeros
+
+        for i, test in enumerate(X):
+            for j, train in enumerate(self.train_X):
+                distances[i][j] = np.abs(train - test).sum()
+        return distances
+
+
+
+    def compute_distances_one_loop(self, X):
+        """
+        Computes L1 distance from every sample of X to every training sample
+        Vectorizes some of the calculations, so only 1 loop is used
+
+        Arguments:
+        X, np array (num_test_samples, num_features) - samples to run
+        
+        Returns:
+        distances, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        """
+
+        distances = np.zeros((X.shape[0], self.train_X.shape[0]))
+        for i in range(X.shape[0]):
+            distances[i] = np.sum(np.abs(X[i] - self.train_X), 1)
+        return distances
+
+
+    def compute_distances_no_loops(self, X):
+        """
+        Computes L1 distance from every sample of X to every training sample
+        Fully vectorizes the calculations using numpy
+
+        Arguments:
+        X, np array (num_test_samples, num_features) - samples to run
+        
+        Returns:
+        distances, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        """
+        return dist.cdist(X, self.train_X, 'cityblock') # Computes Manhattan distance between the points with cityblock metric.
+
+
+    def predict_labels_binary(self, distances):
+        """
+        Returns model predictions for binary classification case
+        
+        Arguments:
+        distances, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        Returns:
+        pred, np array of bool (num_test_samples) - binary predictions 
+           for every test sample
+        """
+
+        n_train = distances.shape[1]
+        n_test = distances.shape[0]
+        prediction = np.zeros(n_test)
+
+        for i in range(n_test):
+            idx = distances[i].argsort()[:self.k]
+            # для каждой i-той строки массива "distances" создать массив индексов его
+            # (элементов по возрастанию). Выбрать из него срез с 0 по k элемент
+            labels = self.train_y[idx]
+            lb_count = np.bincount(labels) # количество вхождений каждого значения labels в массив
+            prediction[i] = np.argmax(lb_count) # позиция максимального элемента массива
+        return prediction
+
+    def predict_labels_multiclass(self, distances):
+        """
+        Returns model predictions for multi-class classification case
+        
+        Arguments:
+        distances, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        Returns:
+        pred, np array of int (num_test_samples) - predicted class index 
+           for every test sample
+        """
+
+        n_train = distances.shape[0]
+        n_test = distances.shape[0]
+        prediction = np.zeros(n_test, np.int)
+
+        for i in range(n_test):
+            idx = distances[i].argsort()[:self.k]
+            labels = self.train_y[idx]
+            lb_count = np.bincount(labels)
+            prediction[i] = np.argmax(lb_count)
+        return prediction
